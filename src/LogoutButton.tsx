6a9150e8654826720319c28bcc16d6c1002144f4
Pedro Barrera <pemamsn@gmail.com>
2026-04-08 16:26:50 +0200

    [PATCH]

--- src/LogoutButton.tsx
+++ src/LogoutButton.tsx
@@ -1,5 +1,4 @@
 import useWalletTwo from "../lib/hooks/useWalletTwo";
-import SignatureAction from "../lib/components/actions/SignatureAction";
 
 export default function LogoutButton() {
   const { user, signMessage, logout } = useWalletTwo();
@@ -10,7 +9,7 @@ export default function LogoutButton() {
     <button className="bg-red-500 text-white px-4 py-2 rounded ml-4" onClick={logout}>
       Logout
     </button>
-    <button className="bg-blue-500 text-white px-4 py-2 rounded ml-4" onClick={() => signMessage("Hello from Wallettwo SDK!")}>
+    <button className="bg-blue-500 text-white px-4 py-2 rounded ml-4" onClick={async () => console.log(await signMessage("Hello from Wallettwo SDK!"))}>
       Sign Message
     </button>
   </>);
